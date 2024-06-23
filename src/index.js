e396fbb75e1d7b911e3760ba49fc84d62b8eabec
LizzieKeegan <lizziekeegan16@gmail.com>
2024-06-23 17:26:48 +0700

    Temperature and City elements replaced with response data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let roundedTemperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = roundedTemperature;
+  cityElement.innerHTML = response.data.city;
+}
+
 function searchCity(city) {
   let apiKey = "3bt55of44c88990a4f0ababb8d8f6206";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
