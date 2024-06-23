f36f610383a5930301d1cf16800d9f937989905e
LizzieKeegan <lizziekeegan16@gmail.com>
2024-06-23 16:44:50 +0700

    API url in javascript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "3bt55of44c88990a4f0ababb8d8f6206";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
